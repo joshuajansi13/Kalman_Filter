e70c87f48ae4ef60de9ca880e97ada252064d07a
Joshua Canlas <canlas.joshua0121@gmail.com>
2021-03-07 15:32:34 -0700

    completed assignment 1 - KF toy implementation

--- assignment1.py
+++ assignment1.py
@@ -4,7 +4,7 @@ from sim.sim1d import sim_run
 # Simulator options.
 options = {}
 options['FIG_SIZE'] = [8,8]
-options['CONSTANT_SPEED'] = True
+options['CONSTANT_SPEED'] = False
 
 class KalmanFilterToy:
     def __init__(self):
@@ -12,9 +12,14 @@ class KalmanFilterToy:
         self.prev_x = 0
         self.prev_t = 0
     def predict(self,t):
-        prediction = 0
+        dt = t - self.prev_t
+        prediction = self.prev_x + self.v * dt
         return prediction
     def measure_and_update(self,x,t):
+        measured_v = (x - self.prev_x) / (t - self.prev_t)
+        self.v += (measured_v - self.v) * 0.6
+        self.prev_t = t
+        self.prev_x = x
         return
 
 
